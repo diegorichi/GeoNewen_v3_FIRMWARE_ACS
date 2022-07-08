bb5e5cde767af0e8343cd77663365a1945331f94
diego.richi <diego.richi@gmail.com>
2022-07-07 21:33:04 -0300

    adding menu class (tested in arduino uno)

---
+++ menu.cpp
@@ -0,0 +1,118 @@
+#include "Arduino.h"
+#include "menu.h"
+
+Menu::Menu(String aName, void (*showMenu)(),void (*refreshMenu)(), int _menuActual)
+{
+  _name = aName;
+  _showMenu = showMenu;
+  refresh = refreshMenu;
+  menuActual = _menuActual;
+}
+
+void Menu::setUp(Menu *up)
+{
+  _up = up;
+}
+void Menu::setDown(Menu *down)
+{
+  _down = down;
+}
+void Menu::setEnter(Menu *enter)
+{
+  _enter = enter;
+}
+void Menu::setEscape(Menu *escape)
+{
+  _escape = escape;
+}
+
+void Menu::setUpAction(void (*action)())
+{
+  _up_action = action;
+}
+void Menu::setDownAction(void (*action)())
+{
+  _down_action = action;
+}
+
+void Menu::setEnterAction(void (*action)())
+{
+  _enter_action = action;
+}
+void Menu::setEscapeAction(void (*action)())
+{
+  _escape_action = action;
+}
+
+String Menu::getName()
+{
+  return _name;
+}
+
+Menu Menu::up()
+{
+  if (_up_action)
+  {
+    _up_action();
+  }
+  if (_up)
+  {
+    _up->show();
+    return (*_up);
+  }
+  return *this;
+}
+Menu Menu::down()
+{
+  if (_down_action)
+  {
+    _down_action();
+  }
+  if (_down)
+  {
+    _down->show();
+    return (*_down);
+  }
+  return *this;
+}
+
+Menu Menu::enter()
+{
+  if (_enter_action)
+  {
+    _enter_action();
+  }
+  if (_enter)
+  {
+    _enter->show();
+    return (*_enter);
+  }
+  return *this;
+}
+
+Menu Menu::escape()
+{
+  if (_escape_action){
+    _escape_action();
+  }
+  if (_escape){
+    _escape->show();
+    return (*_escape);
+  }  
+  return *this;
+}
+
+void Menu::show()
+{
+  if (_showMenu)
+  {
+    _showMenu();
+  }
+}
+
+void Menu::refresh()
+{
+  if (_refresh){
+    _refresh();
+  }
+}
---
+++ menu.h
@@ -0,0 +1,46 @@
+#ifndef Menu_h
+#define Menu_h
+#include "Arduino.h"
+
+class Menu {
+
+  private:
+    String _name;
+    Menu *_up;
+    Menu *_down;
+    Menu *_enter;
+    Menu *_escape;
+    
+    void (*_up_action)(void);   // puntero a funcion ej:MenuUnoCero
+    void (*_down_action)(void);   // puntero a funcion ej:MenuUnoCero
+    void (*_enter_action)(void);   // puntero a funcion ej:MenuUnoCero
+    void (*_escape_action)(void);   // puntero a funcion ej:MenuUnoCero
+    void (*_showMenu)(void);   // puntero a funcion ej:MenuUnoCero
+    void (*_refresh)(void);   // puntero a funcion ej:MenuUnoCero
+
+  public:
+    int menuActual;
+
+    Menu(String aName, void (*showMenu)(), void (*refreshMenu)(), int _menuActual);
+
+    String getName();
+
+    void setUp(Menu* up);
+    void setDown(Menu* down);
+    void setEnter(Menu* enter);
+    void setEscape(Menu* escape);
+
+    void setUpAction(void (*action)());
+    void setDownAction(void (*action)());
+    void setEnterAction(void (*action)());
+    void setEscapeAction(void (*action)());
+
+    Menu up();
+    Menu down();
+    Menu enter();
+    Menu escape();
+    void show();
+    void refresh();
+
+};
+#endif
